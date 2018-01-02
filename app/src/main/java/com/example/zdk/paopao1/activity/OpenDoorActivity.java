c89545da814fa6eea8eb665bc24b8320f8c0e937
TomismeZ <1840317559@qq.com>
2018-01-02 09:08:37 +0800

    secondly

--- app/src/main/java/com/example/zdk/paopao1/activity/OpenDoorActivity.java
+++ app/src/main/java/com/example/zdk/paopao1/activity/OpenDoorActivity.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,10 +16,13 @@ import android.widget.ImageView;
 
 import com.example.zdk.paopao1.R;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class OpenDoorActivity extends AppCompatActivity {
-    public static final String PREFERENCE_PACKAGE = "edu.cczu.SimplePreference";
+    public static final String PREFERENCE_PACKAGE = "com.myncic.ican";
     public static final String PREFERENCE_NAME = "userInfo";
-    public static int MODE = Context.MODE_WORLD_READABLE + Context.MODE_WORLD_WRITEABLE;
+    public static int MODE =Context.MODE_WORLD_WRITEABLE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,12 +54,38 @@ public class OpenDoorActivity extends AppCompatActivity {
                 }
                 SharedPreferences preferences=c.getSharedPreferences(PREFERENCE_NAME,MODE);
                 String ipAdd=preferences.getString("ipAdd","oa.myncic.com");
+                String securitycode=preferences.getString("security", "");
+                //获取用户登录信息
+                Long selfId=preferences.getLong("selfId", -1);
                 int port=preferences.getInt("port",1234);
                 Log.d("OpenDoorActivity","ipAdd: "+ipAdd);
                 Log.d("OpenDoorActivity","port: "+port);
-
+                Log.d("OpenDoorActivity","selfId: "+selfId);
+                Log.d("OpenDoorActivity","securitycode: "+securitycode);
+//                Check check=new Check(selfId,securitycode);
+//                check.execute();
             }
         });
+    }
+
+    class Check extends AsyncTask<Integer,Integer,String>{
+        Long selfId;
+        String securitycode;
+
+        public Check(Long selfId, String securitycode) {
+            this.selfId = selfId;
+            this.securitycode = securitycode;
+        }
+
+        @Override
+        protected String doInBackground(Integer... integers) {
+            try {
+                URL url=new URL("http://oa.myncic.com/open_door.php?uid="+selfId+"&scode="+securitycode);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 
     @Override
